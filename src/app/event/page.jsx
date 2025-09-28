ad2cfdfa7f120445c0cce592c3155a0562016bb2
Pranav Deshmukh <pranavdeshmukh5454@gmail.com>
2025-09-28 23:06:11 +0530

    first commit

--- src/app/event/page.jsx
+++ src/app/event/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Home() {
+export default function Events() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Home Page
+      Events Page
     </div>
   );
 }
