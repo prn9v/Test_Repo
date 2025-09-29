3336502216d4b256160219c89725e7c73099b84e
Pranav Deshmukh <pranavdeshmukh5454@gmail.com>
2025-09-29 10:30:05 +0530

    Deleted Bhai

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        Bhai Bhai
         {children}
       </body>
     </html>
